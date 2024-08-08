d3484e4cf2696e8f5c7a45f4433fffb4061149d8
Mauricio Romero <mauricionromero@hotmail.com>
2024-08-08 14:41:42 -0300

    fix en issue with &nbsp;

--- src/api/wol.ts
+++ src/api/wol.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import axios from 'axios';
 import { load } from 'cheerio';
 
-// const WEEK = 35;
-
 const router = express.Router();
 
 type WolResponse = {
@@ -17,7 +15,8 @@ router.get<
 WolResponse
 >('/:week', async (req, res) => {
   const week = req.params.week;
-  const url = `https://wol.jw.org/es/wol/meetings/r4/lp-s/2024/${week}`;
+  // const url = `https://wol.jw.org/es/wol/meetings/r4/lp-s/2024/${week}`; // es
+  const url = `https://wol.jw.org/en/wol/meetings/r1/lp-e/2024/${week}`; // en
   const response = await axios.get(url);
   const html = response.data;
   const $ = load(html);
@@ -33,14 +32,14 @@ WolResponse
 
   // Get all elements that has the class "dc-icon--music"
   const musicElements = $('.dc-icon--music').map((index, element) => {
-    return $(element).text();
+    // replace if were &nbsp; with a space
+    return $(element).text().replace(/\u00a0/g, ' ');
   });
 
   // Get a filtered list of elements that match the following criteria:
   // 1. The element starts with a number followed by a dot
   // 2. The element is capitalized
   // 3. The element starts with a number in parentheses (in this case extract and return the text in parentheses)
-
   const filteredNumberedElements = data
     .map((element) => {
       const isNumbered = /^\d+\./.test(element);
@@ -54,10 +53,6 @@ WolResponse
       }
     })
     .filter((element) => element);
-
-  //remove undefined elements
-
-  console.log('Numbered elements: ', filteredNumberedElements);
 
   const meet: { [key: string]: string } = {
     dateAndLecture: `${filteredNumberedElements[0]} | ${filteredNumberedElements[1]}`,
@@ -81,7 +76,6 @@ WolResponse
     if ( /^\d+\./.test(element || '') ) {
       const key = `section-${section}-point-${element?.split('.')[0]}`;
       meet[key] = `${element} (${filteredNumberedElements[index + 1]})`;
-      // meet[key] = element as string;
     }
   },
   );
